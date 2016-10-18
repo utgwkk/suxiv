1dea54fae35bc494e87529f90ede37bca1298f07
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-10-18 20:58:01 +0900

    set public folder path

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'erubis'
 module Suxiv
   class WebApp < Sinatra::Base
     set :erb, escape_html: true
+    set :public_folder, File.expand_path('../static', __FILE__)
 
     get '/' do
       'Hello, world!'
